6c404e9b82ec191fdce699a277ab21c8f7b7ce05
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2020-12-13 19:49:10 +0000

    fixing images not transitioning

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-test
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+panel.addEventListener('click', () => {
+    removeActiveClasse()
+    panel.classList.add('active')
+
+})
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+
+
+    })
+
+}
